b66243b2a353640407534498d941ba1c30b51821
betiniakarandut <betiniakarandut@gmail.com>
2025-02-13 14:05:47 +0100

    main: setting for deployment

--- api/main.py
+++ api/main.py
@@ -3,9 +3,24 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from .routes import meals
-
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
 app = FastAPI(title="Food Tracker API", description="API for tracking meal servings", version="1.0")
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "https://your-render-app-name.onrender.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
